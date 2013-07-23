66177f09cb250c9efe57f822b639cec5f686c4e6
Imani <Imany.mohsen@gmail.com>
2013-07-23 16:15:20 +0430

    display search result, add query change event handler

--- Searcher/Form1.Designer.cs
+++ Searcher/Form1.Designer.cs
@@ -36,10 +36,14 @@
             // 
             // txt_search
             // 
+            this.txt_search.Font = new System.Drawing.Font("B Yekan", 11F);
             this.txt_search.Location = new System.Drawing.Point(158, 15);
             this.txt_search.Name = "txt_search";
-            this.txt_search.Size = new System.Drawing.Size(431, 20);
+            this.txt_search.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txt_search.Size = new System.Drawing.Size(431, 30);
             this.txt_search.TabIndex = 0;
+            this.txt_search.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txt_search.TextChanged += new System.EventHandler(this.txt_search_TextChanged);
             // 
             // btn_search
             // 
@@ -54,11 +58,12 @@
             // 
             // txt_result
             // 
+            this.txt_result.Font = new System.Drawing.Font("B Yekan", 8.25F);
             this.txt_result.Location = new System.Drawing.Point(27, 139);
             this.txt_result.Multiline = true;
             this.txt_result.Name = "txt_result";
             this.txt_result.ReadOnly = true;
-            this.txt_result.Size = new System.Drawing.Size(562, 187);
+            this.txt_result.Size = new System.Drawing.Size(562, 402);
             this.txt_result.TabIndex = 2;
             this.txt_result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
@@ -73,12 +78,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(608, 357);
+            this.ClientSize = new System.Drawing.Size(608, 553);
             this.Controls.Add(this.txt_analyzed);
             this.Controls.Add(this.txt_result);
             this.Controls.Add(this.btn_search);
             this.Controls.Add(this.txt_search);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
--- Searcher/Form1.cs
+++ Searcher/Form1.cs
@@ -32,15 +32,32 @@ namespace Searcher
 
         private void btn_search_Click(object sender, EventArgs e)
         {
+
             Query q = text_parser.Parse(txt_search.Text);
             txt_analyzed.Text = q.ToString();
-            var result = searcher.Search(q,1);
+            var result = searcher.Search(q,10);
+            txt_result.Clear();
             foreach (var res in result.ScoreDocs)
             {
                 var resdoc = searcher.Doc(res.Doc);
-                txt_result.Text = res.Score + "  " + resdoc.GetField("text").StringValue + "\n";
+                txt_result.Text += "عنوان: " + resdoc.GetField("title").StringValue + "\r\n";
+                if (resdoc.GetField("type").StringValue != "title")
+                {
+                    txt_result.Text += "  " + resdoc.GetField("text").StringValue;
+                    
+                }
+                txt_result.Text += Environment.NewLine + "--------------------------------" + Environment.NewLine;
+                txt_result.Refresh();
+                
             }
             
+        }
+
+        private void txt_search_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_search.Text.Length < 1)
+                return;
+            btn_search_Click(sender, e);
         }
     }
 }
