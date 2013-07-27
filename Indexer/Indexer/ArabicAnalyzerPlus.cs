4b7f9d3555dbb86543eeece8df215116a32d55ae
Imani <Imany.mohsen@gmail.com>
2013-07-27 12:09:34 +0430

    add stopWord filter to arabicAnalyzerPlus

--- Indexer/Indexer/ArabicAnalyzerPlus.cs
+++ Indexer/Indexer/ArabicAnalyzerPlus.cs
@@ -12,13 +12,16 @@ namespace Indexer
     public class ArabicAnalyzerPlus : Analyzer
     {
         private readonly Lucene.Net.Util.Version _version;
-        public ArabicAnalyzerPlus(Lucene.Net.Util.Version version)
+        private ISet<string> stopWords;
+        public ArabicAnalyzerPlus(Lucene.Net.Util.Version version, ISet<string> sw)
         {
             _version = version;
+            stopWords = sw;
         }
         public override TokenStream TokenStream(string fieldName, System.IO.TextReader reader)
         {
             TokenStream result = new ArabicLetterTokenizer(reader);
+            result = new StopFilter(true, result, stopWords);
             result = new ArabicPlusNormalizationFilter(result);
             result = new ArabicNormalizationFilter(result);
             result = new ArabicStemFilter(result);
